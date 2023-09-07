cd0c930841ada81c863dcc07ac4ceafa58df1315
Lemontea010 <andy.lmtik.lmt@gmail.com>
2023-09-07 21:58:33 +0800

    Hello, i am under the water bububububu

--- src/main/java/Lab1/myLibrary.java
+++ src/main/java/Lab1/myLibrary.java
@@ -4,7 +4,7 @@ public class myLibrary {
     public static int Power(int Base, int Exponent) {
         if(Exponent==1)
             return Base * Exponent;
-        eelse
+        else
         return Base * Power(Base,Exponent-1);
     }
     public static int factorial(int n){
