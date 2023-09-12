527f5771a4e790c3ec6cb9cac879cd5168e2f7e0
Lemontea010 <andy.lmtik.lmt@gmail.com>
2023-09-12 13:52:42 +0800

    Adding the 3rd line of message

--- src/main/java/Lab1/mainApp1.java
+++ src/main/java/Lab1/mainApp1.java
@@ -6,6 +6,7 @@ public class mainApp1 {
         int n=11;
         System.out.println("Welcome to Scientific Calculator!");
         System.out.println("Program started ...");
+        System.out.println("This is the 3rd line of message");
         System.out.println(b+" tp power "+n+" = "+ myLibrary.Power(b,n));
         System.out.println(n+"!="+myLibrary.factorial(n));
     }
